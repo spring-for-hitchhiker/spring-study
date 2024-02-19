124398c29b7b6cb8ce00e6e8a75088f90d681f0c
AreSain <tmdtm9949@gmail.com>
2024-02-19 16:04:14 +0900

    [레시피 1-6] 그레이들 래퍼로 스프링 애플리케이션 빌드하기

---
+++ AreSain/spring5recipe/src/main/java/springstudy/spring5recipe/Spring5recipeApplication.java
@@ -0,0 +1,13 @@
+package springstudy.spring5recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5recipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5recipeApplication.class, args);
+	}
+
+}
---
+++ AreSain/spring5recipe/src/test/java/springstudy/spring5recipe/Spring5recipeApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.spring5recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5recipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
