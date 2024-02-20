cc4c91089ea367e8322c5ade58dee091015e838a
AreSain <tmdtm9949@gmail.com>
2024-02-20 10:56:11 +0900

    [레시피 2-1] 자바로 POJO 구성하기 - 1

--- AreSain/spring5recipe/src/main/java/springstudy/spring5recipe/Spring5recipeApplication.java
+++ AreSain/spring5recipe/src/main/java/springstudy/spring5recipe/Spring5recipeApplication.java
@@ -2,12 +2,23 @@ package springstudy.spring5recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springstudy.spring5recipe.sequence.SequenceGenerator;
+import springstudy.spring5recipe.sequence.config.SequenceGeneratorConfiguration;
 
 @SpringBootApplication
 public class Spring5recipeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5recipeApplication.class, args);
-	}
+		ApplicationContext context =
+			new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
 
+		SequenceGenerator generator = context.getBean(SequenceGenerator.class);
+
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());
+	}
 }
