2c06648bb4f5b0d1cff0b8b1c1ce282b7e7cf689
jiwojung <skyqkqk1168@gmail.com>
2024-02-22 01:15:44 +0900

    1주차과제 #1

---
+++ kenWR/spring5recipeChapter1/src/test/java/com/gaepo/spring5recipe/Spring5RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaepo.spring5recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
