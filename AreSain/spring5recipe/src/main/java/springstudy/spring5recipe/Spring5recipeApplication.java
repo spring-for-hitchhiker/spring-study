a6553639dd0206399fae6a2fa8c8cfa8a0e58d09
AreSain <tmdtm9949@gmail.com>
2024-02-21 19:19:40 +0900

    [레시피 2-2] 생성자 호출해서 POJO 생성하기

--- AreSain/spring5recipe/src/main/java/springstudy/spring5recipe/Spring5recipeApplication.java
+++ AreSain/spring5recipe/src/main/java/springstudy/spring5recipe/Spring5recipeApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import springstudy.spring5recipe.sequence.SequenceDao;
-import springstudy.spring5recipe.sequence.SequenceGenerator;
+import springstudy.spring5recipe.shop.Product;
+import springstudy.spring5recipe.shop.config.ShopConfiguration;
 
 @SpringBootApplication
 public class Spring5recipeApplication {
@@ -14,11 +14,12 @@ public class Spring5recipeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5recipeApplication.class, args);
 		ApplicationContext context =
-			new AnnotationConfigApplicationContext("springstudy.spring5recipe.sequence");
+			new AnnotationConfigApplicationContext(ShopConfiguration.class);
 
-		SequenceDao sequenceDao = context.getBean(SequenceDao.class);
+		Product aaa = context.getBean("aaa", Product.class);
+		Product cdrw = context.getBean("cdrw", Product.class);
 
-		System.out.println(sequenceDao.getNextValue("IT"));
-		System.out.println(sequenceDao.getNextValue("IT"));
+		System.out.println(aaa);
+		System.out.println(cdrw);
 	}
 }
